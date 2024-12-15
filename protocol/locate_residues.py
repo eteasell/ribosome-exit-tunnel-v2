da30b5f4dc92f08b91d7760b2364e163c4486039
eteasell <teasella@gmail.com>
2024-12-15 15:21:35 -0800

    adding comments to locate_residues

--- protocol/locate_residues.py
+++ protocol/locate_residues.py
@@ -22,31 +22,42 @@ def locate_residues(landmark: Landmark,
     '''
     This method takes a landmark centered on the alignment, and finds this residue on the given rcsb_id's polymer.
     Returns the residues position and coordinates.
+    
+    landmark: the landmark to be located
+    polymer: the poymer on which this landmark lies
+    polymer_id: the polymer id specific to this rcsb_id
+    rcsb_id: the id of the ribosome instance
+    chain: the biopython Chain object holding the sequence
+    flat_seq: from SequenceMappingContainer, tuple holding (seq, flat_index_to_residue_map, auth_seq_id_to_flat_index_map)
+    kingdom: kingdom to which this rcsb_id belongs, or none if being called from main_universal.py
     '''
     
+    # access aligned sequence from alignment files
     if kingdom is None:
         path = f"data/output/fasta/aligned_sequences_{polymer}.fasta"
     else:
         path = f"data/output/fasta/aligned_sequences_{kingdom}_{polymer}.fasta"
-        
     alignment = AlignIO.read(path, "fasta")
     aligned_seq = get_rcsb_in_alignment(alignment, rcsb_id)
     
+    # find the position of the landmark on the original riboXYZ seq
+    alignment_position = map_to_original(aligned_seq, landmark.position) 
+    
+    # access riboXYZ sequence (pre alignment)
     orig_seq = check_fasta_for_rcsb_id(rcsb_id, polymer, kingdom)
 
     if orig_seq is None:
         print("Cannot access uniprot sequence")
         return
     
+    # run pairwise alignment on the riboXYZ sequence and the flattened PDB sequence
     alignment = run_pairwise_alignment(rcsb_id, polymer_id, orig_seq, flat_seq[0])
     
     if alignment is None:
         return None
-    
+        
+    # map the alignment_position from the original riboXYZ sequence to the pairwise-aligned flattened PDB sequence
     flattened_seq_aligned = alignment[1]
-    
-    alignment_position = map_to_original(aligned_seq, landmark.position) 
-     
     flat_aligned_position = None
     if alignment_position is not None:  
         flat_aligned_position = map_to_original(flattened_seq_aligned, alignment_position)
@@ -55,25 +66,26 @@ def locate_residues(landmark: Landmark,
         print(f"Cannot find {landmark} on {rcsb_id} {polymer}")
         return None 
         
+    # use the MappingSequenceContainer flat_index_to_residue_map to access to residue in the PDB sequence
     resi_id = flat_seq[1][flat_aligned_position].get_id()
-
     residue = chain[resi_id]
     
+    # check that the located residue is the same as the landmark
     landmark_1_letter = landmark.residue.upper()
     landmark_3_letter = ResidueSummary.one_letter_code_to_three(landmark_1_letter)
     if (residue.get_resname() != landmark_1_letter and residue.get_resname() != landmark_3_letter):
         return None
         
+    # find atomic coordinates for the selected residue
     atom_coords = [atom.coord for atom in residue]
-        
     if (len(atom_coords) == 0): 
        return None
     
+    # take the mean coordinate for the atoms in residue
     vec = np.zeros(3)
     for coord in atom_coords:
         tmp_arr = np.array([coord[0], coord[1], coord[2]])
         vec += tmp_arr
-
     vec = vec / len(atom_coords)
     vec = vec.astype(np.int32)
         
@@ -123,11 +135,23 @@ def cherry_pick(polymer: str,
                 kingdom : str = None
                 ) -> list[Landmark]:
     
-    name_arr = seq.name.split('_')
-    parent = name_arr[1]
-    chain_id = name_arr[2]
-    mapped_conserved = []
-    landmark_number = 0
+    '''
+    This method choses landmarks based on distance to the MOLE model tunnel centerline on the prototype specified by seq
+    
+    polymer: the chain of interest (eg. uL4)
+    seq: a Seq object which contains the protoype aligned sequence
+    conserved_positions: list of Landmark candidates
+    threshold: conservation threshold between [0,1]
+    chain: the chain sequence from the structural PDB file (may contain gaps)
+    flat_seq: outputted from SequenceMappingContainer(chain), chain with all gaps removed, but original indices stored
+    kingdom: one of eukaryota, bacteria, archaea, or None
+    '''
+    
+    name_arr            = seq.name.split('_')
+    parent              = name_arr[1]
+    chain_id            = name_arr[2]
+    mapped_conserved    = []
+    landmark_number     = 0
     
     for i, pos in enumerate(conserved_positions):
         
